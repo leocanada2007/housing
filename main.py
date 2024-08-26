c38311edc91f27870836a923725dfc166ca77736
leocanada2007 <71792477+leocanada2007@users.noreply.github.com>
2024-08-25 21:33:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -211,9 +211,9 @@ def fixed_variable_rate(df,P,a):
 
 def tab_commission():
   price = st.number_input("Enter Purchase/Sale Price", value = 1000000)
-  first_10k = st.number_input("Enter Coop Commission (aka Buyer Agent Commission) on First $100,000 in %", value = 3.255)
+  first_10k = st.number_input("Enter Coop Commission (aka Buyer Agent Commission) on First $100,000 in %", value = 3.255, format = %.3f)
   first_10k = first_10k/100
-  remaining = st.number_input("Enter Coop Commission (aka Buyer Agent Commission) on Balance in %", value = 1.1625)
+  remaining = st.number_input("Enter Coop Commission (aka Buyer Agent Commission) on Balance in %", value = 1.1625, format = %.3f)
   remaining = remaining/100  
  
   if price<=100000:
